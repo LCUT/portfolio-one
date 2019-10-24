bfefe955e0530ec0f38fc3f8d18fb9b288f2d7a1
Lexi Crandall <lexicrandall@Lexis-MacBook-Pro.local>
2019-10-24 11:16:22 -0600

    adding star wars assets and file structure

---
+++ js/starships.js
@@ -0,0 +1,22 @@
+import { starships } from '../assets/starships.js'
+
+let contentArea = document.querySelector
+    ('.content')
+
+starships.forEach(ship => {
+    let shipDiv = document.createElement('div')
+    let shipName = document.createElement('h3')
+    let shipPic = document.createElement('img')
+
+    shipName.textContent = ship.name
+
+    let shipNum = getCharNumber(ship.url)
+
+    shipPic.src = 'https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg'
+
+    shipDiv.appendChild(shipName)
+    shipDiv.appendChile(shipPic)
+    contentArea.appendChild(shipDiv)
+})
+
+function get Char
