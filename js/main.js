c6994c39cc752154be6249a2e9423415346f5970
Lexi Crandall <lexicrandall@Lexis-MacBook-Pro.local>
2019-10-24 19:25:41 -0600

    adding img and fixing links

---
+++ js/main.js
@@ -0,0 +1,102 @@
+import { films } from '/assets/filsm.js'
+import { people } from '/assets/people.js'
+
+let mainArea = document.querySelector('main')
+
+films.forEach(function(film) {
+    let filmDiv = document.createElement('div')
+    let title = document.createElement('h1')
+    let crawl = document.createElement('p')
+
+    filmDiv.appendChild(title)
+    filmDiv.appendChild(crawl)
+
+    title.textContent = film.title
+    crawl.innerText = film.opening_crawl
+
+    mainArea.appendChild(filmDiv)
+})
+
+
+
+people.forEach(function(person) {
+    let personDiv = document.createElement('div')
+    let name = document.createElement('h1')
+    let gender = document.createElement('h3')
+    let pic = document.createElement('img')
+
+    personDiv.appendChild(name)
+    personDiv.appendChild(gender)
+    personDiv.appendChild(pic)
+
+    let charNum = getCharNumber(person.URL)
+
+    name.textContent = person.name
+    gender.textContent = person.gender
+
+    pic.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+    mainArea.appendChild(personDiv)
+
+})
+
+function getCharNumber(charURL) {
+    let end = charURL.lastIndexOf('/')
+    let charID = charURL.substring(end -2, end)
+    if(charID.indexOf('/') !== -1 ) {
+        return charID.slice(1,2)
+    } else {
+        return charID
+    }
+}
+
+
+
+
+const maleCharacters = people.filter(person => person.gender === 'male')
+console.log(maleCharacters)
+const femaleCharacters = people.filter(person => person.gender === 'female')
+console.log(femaleCharacters)
+
+
+
+
+
+
+
+let maleButton = document.createElement('button')
+maleButton.textcontent = "Male Characters"
+maleButton.addEventListener('click', () => {
+    maleCharacters.forEach(elt => {
+        let matchedDiv = allDivs.filter(element => {
+            return element.firstChild.textContent === elt.name
+        })
+        console.log(matchedDiv)
+        matchedDiv[0].setAttribute("style", "display: none;")
+    })
+    femaleCharacters.forEach(elt => {
+        let matchedDiv = allDivs.filter(elemnt => {
+            return element.firstChild.textContent === elt.name
+        })
+        console.log(matchedDiv)
+        matchedDiv[0].setAttribute("style", "display: none;")
+    })
+})
+
+let femaleButton = document.createElement('button')
+femaleButton.textContent = "Female Characters"
+femaleButton.addEventListener('click', event => {
+
+})
+
+mainHeader.appendChild(maleButton)
+mainHeader.appendChild(femaleButton)
+
+personDiv.appendChild(gender)
+personDiv.appendChild(pic)
+
+mainArea.appendChild(personDiv)
+
+
+
+
+const maleCharacters = people.filter(person => person.gender === 'male')
