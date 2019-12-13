52fa2b0a5093379514927da53f78ee8e161900f4
Lexi Crandall Bradshaw <39276266+LCUT@users.noreply.github.com>
2019-12-12 20:22:33 -0700

    trying to fix errors

--- js/testingtestingtest.js
+++ js/testingtestingtest.js
@@ -1,26 +1,27 @@
+import { films } from '../assets/films.js'
 import { people } from '../assets/people.js'
 
-
+console.log("Hey I am Javascript on your page")
 
 let mainArea = document.querySelector("main")
-let mainHeader = document.querySelector("header")
 
+let mainHeader = document.querySelector("header")
 
 
 people.forEach(function(person) {
     let personDiv = document.createElement("div")
-    let name = document.createElement("h2")
+    let name = document.createElement("h1")
     let gender = document.createElement("h3")
     let pic = document.createElement("img")
 
-    personDiv.appendChild(pic)
     personDiv.appendChild(name)
     personDiv.appendChild(gender)
-    
+    personDiv.appendChild(pic)
 
     let charNum = getCharNumber(person.url)
 
- 
+    
+
     name.textContent = person.name
     gender.textContent = person.gender
 
@@ -31,24 +32,21 @@ people.forEach(function(person) {
 
 
 
-function getCharNumber(charURL) {
+
+
+ function getCharNumber(charURL) {
     let end = charURL.lastIndexOf("/")
     let charID = charURL.substring(end -2, end)
-    if(charID.indexOf("/") !== -1 ) {
-        return charID.slice(1,2)
-    } else {
-        return charID
-    }
-}
-
+     if(charID.indexOf("/") !== -1 ) {
+         return charID.slice(1,2)
+     } else {
+         return charID
+     }
+ }
 
 const maleCharacters = people.filter(person => person.gender === "male")
 const femaleCharacters = people.filter(person => person.gender === "female")
-//const otherCharacters = people.filter(person => person.gender !== 'female' && person.gender !== 'male')
 const allDivs = Array.from(mainArea.querySelectorAll("div"))
-
-
-
 
 let maleButton = document.createElement("button")
 maleButton.textContent = "Male Characters"
@@ -57,42 +55,27 @@ maleButton.addEventListener('click', () => {
         let matchedDiv = allDivs.filter(element => {
             return element.firstChild.textContent === elt.name
         })
-    matchedDiv[0].setAttribute("style","display:none;" )
-    });
+        console.log(matchedDiv)
+    matchedDiv[0].setAttribute("style", "display: none;" )
+  });
 } )
-
 let femaleButton = document.createElement("button")
 femaleButton.textContent = "Female Characters"
 femaleButton.addEventListener('click', () => {
     femaleCharacters.forEach(elt => {
-        let matchedDiv = allDivs.find(element => {
+        let matchedDiv = allDivs.filter(element => {
             return element.firstChild.textContent === elt.name
         })
-        matchedDiv[0].setAttribute("style","display:none;")
-    })
+        console.log(matchedDiv)
+    matchedDiv[0].setAttribute("style", "display: none;" )
+        
+        // elt.setAttribute("style", "visibility: hidden;")
+  });
 })
-
-//let otherButton = document.createElement('button')
-//otherButton.textContent = "Other Characters"
-//otherButton.addEventListener('click', () => {
-  //femaleCharacters.forEach(character => {
-   // let matchedDiv = allDivs.filter(element => {
-     // return element.firstChild.textContent === elt.name
-    //})
-    //matchedDiv[0].setAttribute("style","display:none;");
-    
-  //})
-//})
-
-
-
-//mainHeader.appendChild(otherButton)
 mainHeader.appendChild(maleButton)
 mainHeader.appendChild(femaleButton)
 
-
-
-
-
-
-
+// const maleCharacters = people.filter(person => person.gender === "male")
+// console.log(maleCharacters)
+// const femaleCharacters = people.filter(person => person.gender === "female")
+// console.log(femaleCharacters)
